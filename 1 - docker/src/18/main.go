bab67f8e0d2fa31dafbc7737b66fda16e0fbbaec
Adrian Eka <eekkaaadrian@gmail.com>
2020-04-26 02:56:40 +0700

    finish 18

--- 1 - docker/src/18/main.go
+++ 1 - docker/src/18/main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"github.com/go-redis/redis"
 	"net/http"
 	"os"
+
+	"github.com/go-redis/redis"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func testConnectRedis() {
 	redisHost := os.Getenv("REDIS_HOST")
 	redisPort := os.Getenv("REDIS_PORT")
+
+	fmt.Println("Trying to connect redis", redisHost, ":", redisPort)
 
 	client := redis.NewClient(&redis.Options{
 		Addr:     redisHost + ":" + redisPort,
@@ -19,23 +25,57 @@ func testConnectRedis() {
 
 	_, err := client.Ping().Result()
 
-	if err == nil {
-		fmt.Println("Successfully Connect Redis...")
-		return
+	if err != nil {
+		panic(err)
+
 	}
 
-	fmt.Println(err)
+	fmt.Println("Successfully Connect Redis...")
+}
+
+func testConnectMongo() {
+	mongoHost := os.Getenv("MONGO_HOST")
+	mongoPort := os.Getenv("MONGO_PORT")
+	ctx := context.Background()
+
+	fmt.Println("Trying to connect mongo", mongoHost, ":", mongoPort)
+
+	clientOptions := options.Client()
+	clientOptions.ApplyURI("mongodb://" + mongoHost + ":" + mongoPort)
+
+	client, err := mongo.NewClient(clientOptions)
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = client.Connect(ctx)
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully Connect Mongo...")
 }
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		testConnectRedis()
+		testConnectMongo()
+		fmt.Println("----------------------------------------------------")
 		fmt.Fprintf(w, "Hello World "+os.Getenv("NAME")+"!")
 	})
 
 	appPort := os.Getenv("APP_PORT")
 
-	err := http.ListenAndServe(":" + appPort, nil)
+	fmt.Println("Starting server on ", appPort)
+	err := http.ListenAndServe(":"+appPort, nil)
 
 	panic(err)
 }
--- 1 - docker/src/18/runMe.sh
+++ 1 - docker/src/18/runMe.sh
@@ -6,10 +6,18 @@ originalRedisPort="6379"
 originalMongoPort="27017"
 
 redisPort="6380"
-mongoPort="27017"
+mongoPort="27018"
 
 redisContainerName="redis"
 mongoContainerName="mongo"
+
+appName="app-docker-network"
+appTag="1.0"
+appImage="$appName:$appTag"
+appContainerName="app-docker-network-container"
+appPort="9090"
+
+dockerNetworkName="golang_network"
 
 echo "Getting Required Golang Dependencies..."
 dep ensure -v
@@ -24,18 +32,34 @@ sudo docker pull $mongoImage
 echo "Stoping existing container...."
 sudo docker container stop $redisContainerName
 sudo docker container stop $mongoContainerName
+sudo docker container stop $appContainerName
 
 echo "Removing existing container...."
 sudo docker container rm $redisContainerName
 sudo docker container rm $mongoContainerName
+sudo docker container rm $appContainerName
+
+echo "Removing and building application image"
+sudo docker image rm $appImage
+sudo docker build --tag $appName:$appTag .
 
 echo "Creating container...."
 sudo docker container create --name $redisContainerName -p $redisPort:$originalRedisPort $redisImage
 sudo docker container create --name $mongoContainerName -p $mongoPort:$originalMongoPort $mongoImage
+sudo docker container create --name $appContainerName -p $appPort:$appPort $appImage
 
 echo "Starting container"
 sudo docker container start $redisContainerName
 sudo docker container start $mongoContainerName
+sudo docker container start $appContainerName
+
+echo "Creating and setting network...."
+sudo docker network rm $dockerNetworkName
+sudo docker network create $dockerNetworkName
+sudo docker network connect $dockerNetworkName $redisContainerName
+sudo docker network connect $dockerNetworkName $mongoContainerName
+sudo docker network connect $dockerNetworkName $appContainerName
 
 echo "Showing running container..."
 sudo docker container ls
+sudo docker logs -f $appContainerName
