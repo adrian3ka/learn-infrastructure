2f9fd4a55f45e758a21e0831d57daf2a4e0390a9
Adrian Eka <eekkaaadrian@gmail.com>
2020-05-01 00:23:59 +0700

    add airflow

---
+++ 2 - kubernetes/Basic/src/8/install.sh
@@ -0,0 +1,31 @@
+sudo add-apt-repository multiverse && sudo apt-get update
+sudo apt install virtualbox
+
+echo "--------------Check virtual box version-------------"
+vboxmanage --version
+
+echo "-----------------Installing kubectl-----------------"
+sudo rm /usr/local/bin/kubectl
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+
+chmod +x ./kubectl
+
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+./../resource/minikube-linux-amd64 delete
+
+echo "----------------Starting minikube-------------------"
+
+./../resource/minikube-linux-amd64 start
+
+echo "---------------CHECK KUBE VERSION-------------------"
+kubectl version
+echo "------------------CREATING POD----------------------"
+kubectl create -f nginx-with-label.yaml
+
+kubectl get pod --show-labels
+
+kubectl describe pod nginx-with-label
+
+echo "Please run 'kubectl port-forward nginx 8888:80' when status is running"
