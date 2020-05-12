c41a18eb0e618bb1cbb696bfe4360a416f50853d
Adrian Eka <eekkaaadrian@gmail.com>
2020-05-12 12:44:47 +0700

    update unused

---
+++ 6 -  vault/3. Application Integration/Example/postgres-vault-dynamic-credentials/src/test/java/com/example/postgresvaultdynamiccredentials/PostgresVaultDynamicCredentialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postgresvaultdynamiccredentials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresVaultDynamicCredentialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
